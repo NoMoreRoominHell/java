ecc91d6e6883f59082402007aaa642200269ac14
unknown <zfang123@icloud.com>
2018-04-23 13:48:05 +0800

    优化

--- accumulate/accumulate-admin/src/main/java/com/accumulate/utils/SftpUtils.java
+++ accumulate/accumulate-admin/src/main/java/com/accumulate/utils/SftpUtils.java
@@ -2,12 +2,8 @@ package com.accumulate.utils;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.Properties;
-import java.util.UUID;
 import java.util.Vector;
-
-import org.springframework.web.multipart.MultipartFile;
 
 import com.jcraft.jsch.Channel;
 import com.jcraft.jsch.ChannelSftp;
@@ -15,11 +11,11 @@ import com.jcraft.jsch.ChannelSftp.LsEntry;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
+import com.jcraft.jsch.SftpATTRS;
 import com.jcraft.jsch.SftpException;
 
 public class SftpUtils {
 
-	// 配置文件
 	private static Properties properties;
 
 	static {
@@ -31,12 +27,6 @@ public class SftpUtils {
 		}
 	}
 
-	/**
-	 * 获取sftp连接
-	 * 
-	 * @return
-	 * @throws JSchException
-	 */
 	public static ChannelSftp getChannel() throws JSchException {
 		String host = properties.getProperty("sftp.host");
 		int port = Integer.parseInt(properties.getProperty("sftp.port"));
@@ -67,70 +57,125 @@ public class SftpUtils {
 	}
 
 	/**
-	 * 文件上传
+	 * 创建单文件夹
 	 * 
-	 * @param file
+	 * @param folder
 	 * @return
-	 * @throws IOException
-	 * @throws SftpException
-	 * @throws JSchException
 	 */
-	public static String uploadFile(MultipartFile file) {
-		InputStream is = null;
+	public static boolean mkdir(String folder) {
+		ChannelSftp sftp = null;
 		try {
-			is = file.getInputStream();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return uploadFile(file.getOriginalFilename(), is);
-	}
-
-	/**
-	 * 文件上传
-	 * 
-	 * @param originalFilename：本地文件名
-	 * @param is：上传文件流
-	 * @return filename：保存在服务器上的文件名
-	 * @throws JSchException
-	 * @throws SftpException
-	 */
-	public static String uploadFile(String originalFilename, InputStream is) {
-		return uploadFile(originalFilename, is, properties.getProperty("sftp.activity.path"));
-	}
-
-	/**
-	 * 
-	 * @Description:上传 用原始名称 不另外生成名称
-	 * @author yangling
-	 * @date 2018年3月2日下午4:03:28
-	 */
-	public static String uploadFileUseOriginalName(String originalFilename, InputStream is) {
-		ChannelSftp channel = null;
-		try {
-			channel = getChannel();
-			channel._put(is, properties.getProperty("sftp.bank.path") + originalFilename, null, 0);
+			sftp = getChannel();
+			sftp.mkdir(properties.getProperty("sftp.elec.contract.path") + folder);
 		} catch (JSchException e) {
 			e.printStackTrace();
 		} catch (SftpException e) {
 			e.printStackTrace();
+		} finally {
+			close(sftp);
 		}
-		close(channel);
-		return originalFilename;
+		return true;
 	}
-	
+
 	/**
-	 * 文件上传
+	 * 创建目录
 	 * 
-	 * @param originalFilename：本地文件名
-	 * @param is：上传文件流
-	 * @param path：文件保存路径
-	 * @return filename：保存在服务器上的文件名
-	 * @throws JSchException
-	 * @throws SftpException
+	 * @param createpath
+	 * @return
+	 */
+	public static boolean createDir(String createpath) {
+		ChannelSftp sftp = null;
+		try {
+			sftp = getChannel();
+			if (isExist(createpath)) {
+				sftp.cd(createpath);
+				return true;
+			}
+			String pathArry[] = createpath.split("/");
+			StringBuffer filePath = new StringBuffer("/");
+			for (String path : pathArry) {
+				if (path.equals("")) {
+					continue;
+				}
+				filePath.append(path + "/");
+				if (isExist(filePath.toString())) {
+					sftp.cd(filePath.toString());
+				} else {
+					// 建立目录
+					sftp.mkdir(filePath.toString());
+					// 进入并设置为当前目录
+					sftp.cd(filePath.toString());
+				}
+
+			}
+			sftp.cd(createpath);
+			return true;
+		} catch (SftpException e) {
+			e.printStackTrace();
+		} catch (JSchException e) {
+			e.printStackTrace();
+		} finally {
+			close(sftp);
+		}
+		return false;
+	}
+
+	/**
+	 * 获取目录列表
+	 * 
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public static Vector<LsEntry> listFiles() {
+		ChannelSftp sftp = null;
+		try {
+			sftp = getChannel();
+			return (Vector<LsEntry>) sftp.ls(properties.getProperty("sftp.elec.contract.path"));
+		} catch (JSchException e) {
+			e.printStackTrace();
+		} catch (SftpException e) {
+			e.printStackTrace();
+		} finally {
+			close(sftp);
+		}
+		return null;
+	}
+
+	/**
+	 * 该文件夹是否存在
+	 * 
+	 * @param folder
+	 * @return
+	 */
+	public static boolean isExist(String folder) {
+		ChannelSftp sftp = null;
+		boolean isDirExistFlag = false;
+		try {
+			sftp = getChannel();
+			SftpATTRS sftpATTRS = sftp.lstat(folder);
+			isDirExistFlag = true;
+			return sftpATTRS.isDir();
+		} catch (Exception e) {
+			if (e.getMessage().toLowerCase().equals("no such file")) {
+				isDirExistFlag = false;
+			}
+		} finally {
+			close(sftp);
+		}
+		return isDirExistFlag;
+	}
+
+	/**
+	 * 上传文件
+	 * 
+	 * @param originalFilename
+	 * @param is
+	 * @param path
+	 * @return
 	 */
 	public static String uploadFile(String originalFilename, InputStream is, String path) {
 		ChannelSftp channel = null;
-		String filename = generateFileUUIDName(originalFilename);
+		String filename = originalFilename;
 		try {
 			channel = getChannel();
 			channel._put(is, path + filename, null, 0);
@@ -138,68 +183,13 @@ public class SftpUtils {
 			e.printStackTrace();
 		} catch (SftpException e) {
 			e.printStackTrace();
+		} finally {
+			close(channel);
 		}
-		close(channel);
 		return filename;
 	}
 
-	/**
-	 * 文件下载
-	 * 
-	 * @param filename
-	 *            ：待下载的文件名（没有扩展名）
-	 * @param os：响应的输出流
-	 * @return filename :待下载的文件名（扩展名）
-	 * @throws JSchException
-	 * @throws SftpException
-	 */
-	public static String downFile(String filename, OutputStream os) {
-		return downFile(filename, os, properties.getProperty("sftp.activity.path"));
-	}
-
-	/**
-	 * 文件下载
-	 * 
-	 * @param filename
-	 *            ：待下载的文件名（没有扩展名）
-	 * @param os：响应的输出流
-	 * @return filename :待下载的文件名（扩展名）
-	 * @throws SftpException
-	 * @throws JSchException
-	 */
-	@SuppressWarnings("rawtypes")
-	public static String downFile(String filename, OutputStream os, String path) {
-		ChannelSftp channel = null;
-		try {
-			channel = getChannel();
-			Vector ls = channel.ls(path);
-			if (ls != null && ls.size() > 0) {
-				for (int i = 0; i < ls.size(); i++) {
-					LsEntry entry = (LsEntry) ls.get(i);
-					if (entry.getFilename().contains(filename)) {
-						filename = entry.getFilename();
-						channel.get(path + filename, os, null);
-						close(channel);
-						return filename;
-					}
-				}
-			}
-		} catch (SftpException e) {
-			e.printStackTrace();
-		} catch (JSchException e) {
-			e.printStackTrace();
-		}
-		close(channel);
-		return null;
-	}
-
-	/**
-	 * 生成保存在服务器上的文件名
-	 * 
-	 * @param originalFilename
-	 * @return
-	 */
-	public static String generateFileUUIDName(String originalFilename) {
-		return UUID.randomUUID().toString().replaceAll("-", "") + originalFilename.substring(originalFilename.indexOf("."));
+	public static Properties getProperties() {
+		return properties;
 	}
 }
