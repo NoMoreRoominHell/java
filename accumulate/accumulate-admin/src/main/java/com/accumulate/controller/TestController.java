663e4f89a8eab277d2c8aa8af3c35315a3640e02
unknown <zfang123@icloud.com>
2018-04-25 15:34:13 +0800

    优化

--- accumulate/accumulate-admin/src/main/java/com/accumulate/controller/TestController.java
+++ accumulate/accumulate-admin/src/main/java/com/accumulate/controller/TestController.java
@@ -1,14 +1,14 @@
 package com.accumulate.controller;
 
-import java.util.List;
+//import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.accumulate.core.entity.QzLog;
+//import com.accumulate.core.entity.QzLog;
 //import com.accumulate.service.IQzLogService;
-import com.github.pagehelper.PageHelper;
+//import com.github.pagehelper.PageHelper;
 
 @Controller
 public class TestController {
