b7b1684e666c3616767795bd535e62a2d6c780a6
unknown <zfang123@icloud.com>
2018-07-16 09:49:27 +0800

    pure version

---
+++ algorithms-fourth/src/test/java/com/algorithms/fourth/AlgorithmsApplictionTests.java
@@ -0,0 +1,15 @@
+package com.algorithms.fourth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlgorithmsApplictionTests {
+	
+	@Test
+	public void contextLoads() {
+	}
+}
