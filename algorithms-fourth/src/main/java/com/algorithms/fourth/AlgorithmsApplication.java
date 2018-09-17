3a55b271a580e9df0cabe370ca1074035c95b416
unknown <zfang123@icloud.com>
2018-09-17 09:08:50 +0800

    connection

--- algorithms-fourth/src/main/java/com/algorithms/fourth/AlgorithmsApplication.java
+++ algorithms-fourth/src/main/java/com/algorithms/fourth/AlgorithmsApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AlgorithmsApplication {
-	
-	public static void main(String[] args) {
-		SpringApplication.run(AlgorithmsApplication.class, args);
-	}
-	
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmsApplication.class, args);
+    }
+
 }
